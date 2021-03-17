1fb89a09dc2061c8dadcf5ae917ee3bb09533352
Louis <louisaltm@gmail.com>
2021-03-17 18:37:43 +0100

    Login Komponente: Design fertig. Service fehlt.

---
+++ src/app/login/logindashboard/logindashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../login.service';
+
+@Component({
+  selector: 'app-logindashboard',
+  templateUrl: './logindashboard.component.html',
+  styleUrls: ['./logindashboard.component.less']
+})
+export class LogindashboardComponent implements OnInit {
+
+  constructor(private fb: FormBuilder, private service: LoginService) { }
+
+  loginForm: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+    username: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required])
+    });
+  }
+
+  
+
+  submitForm() {
+    this.service.login();
+  }
+
+}
