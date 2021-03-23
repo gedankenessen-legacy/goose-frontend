168c74d58b63a5a0b6493e6f47c001cff4114fcf
Louis <louisaltm@gmail.com>
2021-03-23 17:00:14 +0100

    Register: Fertig bis aus Service

---
+++ src/app/register/registerdashboard/registerdashboard.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable, Observer } from 'rxjs';
+import { RegisterService } from '../register.service';
+
+@Component({
+  selector: 'app-registerdashboard',
+  templateUrl: './registerdashboard.component.html',
+  styleUrls: ['./registerdashboard.component.less']
+})
+export class RegisterdashboardComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private service: RegisterService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: new FormControl('',[Validators.required]),
+      password1: new FormControl('', [Validators.required]),
+      password2: new FormControl('', [this.passwordMatch])
+      });    
+  }
+
+  submitForm(){
+    /*if(this.registerForm.get('password1').value === this.registerForm.get('password2').value){
+      this.service.register(this.registerForm.get('name').value, this.registerForm.get('password1').value).subscribe(
+        (data)=>{
+          this.router.navigateByUrl('/')
+        },
+        (error) =>{
+          console.error(error);
+        }
+      )
+    }else{
+      
+    }  */ 
+  }
+
+  passwordMatch = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { error: true, required: true };
+    } else if (control.value !== this.registerForm.controls.password1.value) {
+      return { error: true, passwordMatch: true };
+    }
+    return {};
+  };
+}
