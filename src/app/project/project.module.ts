c2c92ed8f1bec97a5ddda4a40120c9426b23b992
namo2 <Namo_2@gmx.de>
2021-03-27 19:37:52 +0100

    Layout

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -7,6 +7,12 @@ import { NzButtonModule } from "ng-zorro-antd/button";
 import { NzIconModule } from "ng-zorro-antd/icon";
 import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { SettingsComponent } from './settings/settings.component';
+import { FormsModule } from "@angular/forms";
+import { NzSelectModule } from "ng-zorro-antd/select";
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzRadioModule } from "ng-zorro-antd/radio";
+import { NzDropDownModule } from "ng-zorro-antd/dropdown";
 
 
 @NgModule({
@@ -17,7 +23,13 @@ import { SettingsComponent } from './settings/settings.component';
     ProjectRoutingModule,
     NzButtonModule,
     NzIconModule,
-    NzPopoverModule
+    NzPopoverModule,
+    FormsModule,
+    NzSelectModule,
+    NzListModule,
+    NzInputModule,
+    NzRadioModule,
+    NzDropDownModule
   ]
 })
 export class ProjectModule { }
--- src/app/project/settings/settings.component.ts
+++ src/app/project/settings/settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import User from "../../interfaces/User";
+import State from "../../interfaces/project/State";
+import { ProjectService } from "../project.service";
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: ProjectService) { }
 
   ngOnInit(): void {
+
   }
 
+  // Sort functions
+  sortColumnName = (a: User, b: User) => a.lastname.localeCompare(b.lastname);
+
+  // Attributes
+  QAButtonSize: 'default';
+  projectName: string = "Hi";
+
+  // customer
+  selectedCustomer: any;
+  userList: any;
+  isLoading: true;
+
+  // employees
+  employeeList: any;
+  radioValueUserRights: any;
+
+  // States
+  customStateIn: string;
+  selectedPhase: string;
+  customStates: State[];
+  phaseList: string[] = ["Verhandlungsphase", "Bearbeitungsphase", "Abschlussphase"];
+
+  deleteCustomState(id) {
+    // Delete elem
+    let pos = this.customStates.findIndex(state => state.id == id);
+    this.customStates = this.customStates.slice(pos, 1);
+  }
+
+  addCustomState() {
+    let elem: State = {
+      name: this.customStateIn,
+      phase: this.selectedPhase,
+      userGenerated: true
+    }
+
+    // Post elem
+    // Get elem
+    // Add elem to list
+  }
+
+  sendForm() {
+
+  }
+
+  test() {
+
+  }
+
+  onSearch($event: string) {
+
+  }
 }
