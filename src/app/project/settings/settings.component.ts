cba09aeed92223423b3be98a77ed010f54ba746a
namo2 <Namo_2@gmx.de>
2021-03-31 19:01:19 +0200

    ProjektSettings für dieses Arbeitspaket beendet.  - POST/PUT von einem Projekt verfügbar (Nur Projektname)  - Autovervollständigung für den Customer -> User werden in einer Liste angezeigt

--- src/app/project/settings/settings.component.ts
+++ src/app/project/settings/settings.component.ts
@@ -17,14 +17,12 @@ import { ProjectUser } from "../../interfaces/project/ProjectUser";
 })
 export class SettingsComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private projectService: ProjectService, private projectUserService: ProjectUserService, private userService: UserService) { }
+  constructor(private route: ActivatedRoute, private projectService: ProjectService, private projectUserService: ProjectUserService, private userService: UserService) {
+  }
 
   ngOnInit(): void {
     const companyId = this.route.snapshot.paramMap.get('companyId');
     const projectId = this.route.snapshot.paramMap.get('projectId');
-
-    console.log(companyId);
-    console.log(projectId);
 
     this.project.company_id = companyId;
 
@@ -34,22 +32,39 @@ export class SettingsComponent implements OnInit {
       forkJoin([this.getUsers()]).subscribe();
   }
 
-  // Sort functions
+  // Column Sort functions
   sortColumnName = (a: User, b: User) => a.lastname.localeCompare(b.lastname);
 
-  // Attributes
-  QAButtonSize: 'default';
-  project: Project = {name: ""};
+  // Project attributes
+  project: Project = {id: "", name: ""};
   customer: ProjectUser;
   filteredListOfUsers: string[] = [];
 
-  // Data lists
-
-  // customer
+  // Customer attributes
   selectedCustomer: User;
   listOfUsers: User[];
-  userInput: string = '';
-  onChange(value: string): void {
+  userInput: User;
+
+  // Employee attributes
+  employeeList: any;
+  radioValueEmployeeRights: any;
+
+  // CustomState attributes
+  customStateIn: string;
+  selectedPhase: string;
+  customStates: State[];
+  phaseList: string[] = ["Verhandlungsphase", "Bearbeitungsphase", "Abschlussphase"];
+
+  // Customer functions
+  compareCustomer(o1: User | string, o2: User): boolean {
+    if (o1) {
+      return typeof o1 === 'string' ? o1 === (o2.firstname + ' ' + o2.lastname) : (o1.firstname === o2.firstname && o1.lastname === o2.lastname);
+    } else {
+      return false;
+    }
+  };
+
+  onCustomerInputChange(value: string): void {
     this.filteredListOfUsers = [];
 
     this.listOfUsers.filter(option => {
@@ -58,74 +73,34 @@ export class SettingsComponent implements OnInit {
     }).forEach(user => this.filteredListOfUsers.push(user.firstname + ' ' + user.lastname));
   }
 
-  // employees
-  employeeList: any;
-  radioValueUserRights: any;
+  // Employee functions
+  removeEmployee() {}
 
-  // States
-  customStateIn: string;
-  selectedPhase: string;
-  customStates: State[];
-  phaseList: string[] = ["Verhandlungsphase", "Bearbeitungsphase", "Abschlussphase"];
+  // CustomStates functions
+  deleteCustomState(id) {}
+  addCustomState() {}
 
   // Getters
-  private getProject(companyId: string, projectId: string): Observable<any>  {
+  private getProject(companyId: string, projectId: string): Observable<any> {
     return this.projectService.getProject(companyId, projectId).pipe(
       tap(projects => this.project = projects)
     );
   }
 
-  private getUsers(): Observable<any>  {
+  private getUsers(): Observable<any> {
     return this.userService.getUsers().pipe(
       tap(users => {
         this.listOfUsers = users;
-        this.onChange(this.userInput);
       })
     );
   }
 
-
-  deleteCustomState(id) {
-    // return;
-
-    // Delete elem
-    let pos = this.customStates.findIndex(state => state.id == id);
-    this.customStates = this.customStates.slice(pos, 1);
-  }
-
-  addCustomState() {
-    // return;
-
-    let elem: State = {
-      name: this.customStateIn,
-      phase: this.selectedPhase
-    }
-
-    // Post elem
-    // Get elem
-    // Add elem to list
-  }
-
   sendForm() {
-    let customer: User = {
-      firstname: this.userInput.substring(0, this.userInput.indexOf(' ')),
-      lastname: this.userInput.substring(this.userInput.indexOf(' ')+1)
+    // Post / Update Project
+    if (this.project.id !== "") {
+      this.projectService.updateProject(this.project.company_id, this.project.id, this.project).subscribe();
+    } else {
+      this.projectService.createProject(this.project.company_id, this.project).subscribe();
     }
-    console.log(customer)
-    console.log(this.project);
-    // if (this.project.id !== undefined)
-    //   this.projectService.updateProject(this.project.company_id, this.project.id, this.project);
-    // else
-    //   this.projectService.createProject(this.project.company_id, this.project);
   }
-
-  onSearch($event: string) {
-
-  }
-
-  test() {
-
-  }
-
-
 }
