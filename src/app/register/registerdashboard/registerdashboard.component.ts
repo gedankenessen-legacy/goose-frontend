5d42a9963ec42eafd44cfeef9e160af30b03d548
Louis <louisaltm@gmail.com>
2021-04-05 18:35:22 +0200

    Anpassen des Design/ Fehlt Anbindung Backend

--- src/app/register/registerdashboard/registerdashboard.component.ts
+++ src/app/register/registerdashboard/registerdashboard.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, Observer } from 'rxjs';
+import { User } from 'src/app/interfaces/User';
 import { RegisterService } from '../register.service';
+import { RegisterContent } from '../RegisterContent';
+
 
 @Component({
   selector: 'app-registerdashboard',
@@ -11,28 +14,59 @@ import { RegisterService } from '../register.service';
 })
 export class RegisterdashboardComponent implements OnInit {
   registerForm: FormGroup;
+  isVisible = false;
+  retUser: User;
+  
 
   constructor(private fb: FormBuilder, private service: RegisterService, private router:Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
     this.registerForm = this.fb.group({
       companyname: new FormControl('',[Validators.required]),
       firstname: new FormControl('',[Validators.required]),
       lastname: new FormControl('',[Validators.required]),
       password1: new FormControl('', [Validators.required]),
       password2: new FormControl('', [this.passwordMatch])
-      });    
+      }); 
+  }
+
+  handleOk(): void {
+    //console.log('Button ok clicked!');
+    this.isVisible = false;
+    this.router.navigateByUrl['/login'];
+  }
+  
+  handleCancel(): void {
+    this.isVisible = false;
   }
 
   submitForm(){
-    /*this.service.register(this.registerForm.get('name').value, this.registerForm.get('password1').value).subscribe(
-        (data)=>{
-          this.router.navigateByUrl('/')
-        },
-        (error) =>{
-          console.error(error);
+    this.isVisible = true;
+    let registercontent: RegisterContent ={
+      firstname: this.registerForm.get('firstname').value,
+      lastname: this.registerForm.get('lastname').value,
+      password: this.registerForm.get('password1').value,
+      companyName: this.registerForm.get('companyname').value
+    }
+    this.register(registercontent);
+  }
+
+  register(registercontent: RegisterContent){
+    this.service.register(registercontent).subscribe(
+      (data)=>{
+        //this.router.navigateByUrl('/')
+        this.retUser={
+          firstname: data[0].firstname,
+          lastname: data[0].lastname,
+          username: data[0].username,
+          id: data[0].id
         }
-      )*/
+
+      },
+      (error) =>{
+        console.error(error);
+      }
+    )
   }
 
   passwordMatch = (control: FormControl): { [s: string]: boolean } => {
