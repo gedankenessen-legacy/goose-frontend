6840f6f9b66ffe3a88bcfe4b5d7000c78e54954d
Benjamin Kürten <admin@benjaminkuerten.de>
2021-04-05 16:50:08 +0200

    * In Dev

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { BaseService } from '../base.service';
+import { User } from '../interfaces/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
+
+  constructor(private http: HttpClient, private router: Router, private baseService: BaseService) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('token')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }),
+    };
+
+    const body = new HttpParams().set('grant_type', 'password').set('username', username).set('password', password);
+
+    return this.http.post<any>(this.baseService.getUrl + '/TODO', body.toString(), httpOptions)
+      .pipe(map(user => {
+        localStorage.setItem('token', JSON.stringify(user));
+        this.currentUserSubject.next(user);
+        return user;
+      }));
+  }
+
+  logout() {
+    this.router.navigate(['/']);
+    localStorage.removeItem('token');
+    this.currentUserSubject.next(null);
+    return true;
+  }
+
+}
--- src/app/interfaces/User.ts
+++ src/app/interfaces/User.ts
@@ -2,4 +2,5 @@ export interface User {
   id: string;
   firstname: string;
   lastname: string;
+  token?: string;
 }
