23008ff7c131824a82475c90ebf7752a056957df
sfuerder <sfuerder@stud.hs-offenburg.de>
2021-04-07 14:42:03 +0200

    Zusammenführung Conversation und IssueComponent

--- src/app/issue/issue.module.ts
+++ src/app/issue/issue.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { IssueRoutingModule } from './issue-routing.module';
-
 import { ConversationComponent } from './conversation/conversation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzCommentModule } from 'ng-zorro-antd/comment';
@@ -18,7 +17,6 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
-
 
 @NgModule({
   declarations: [DashboardComponent, ConversationComponent, IssueComponent],
@@ -38,7 +36,7 @@ import { NzSpinModule } from 'ng-zorro-antd/spin';
     NzGridModule,
     NzIconModule,
     NzSpinModule,
-    NzTableModule
-  ]
+    NzTableModule,
+  ],
 })
 export class IssueModule {}
