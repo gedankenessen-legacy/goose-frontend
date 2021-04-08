f16ba958fc9bb0ba7729dafb8f71c321c1d8bfd6
namo2 <Namo_2@gmx.de>
2021-04-08 12:00:53 +0200

    CSS allgemein so wie für die Kunden Auswahl gefixed

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -14,6 +14,7 @@ import { NzRadioModule } from "ng-zorro-antd/radio";
 import { NzDropDownModule } from "ng-zorro-antd/dropdown";
 import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { NzGridModule } from "ng-zorro-antd/grid";
 
 
 @NgModule({
@@ -31,7 +32,8 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
     NzInputModule,
     NzRadioModule,
     NzDropDownModule,
-    NzAutocompleteModule
+    NzAutocompleteModule,
+    NzGridModule
   ]
 })
 export class ProjectModule { }
