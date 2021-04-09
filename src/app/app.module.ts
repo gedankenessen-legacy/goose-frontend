17bfae76621a9fa6debe470daa6c5d4a83d70e9b
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-04-09 12:04:24 +0200

    Kopiere Interceptor von blob

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { de_DE } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import de from '@angular/common/locales/de';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { AuthHttpInterceptor } from './AuthHttpInterceptor';
 
 registerLocaleData(de);
 
@@ -30,7 +31,10 @@ registerLocaleData(de);
     NzMenuModule,
     NzGridModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: de_DE }],
+  providers: [
+    { provide: NZ_I18N, useValue: de_DE },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
