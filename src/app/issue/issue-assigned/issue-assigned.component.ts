b34d70c30837d9958a81f3f747dbbf480a93bbca
namo2 <Namo_2@gmx.de>
2021-04-13 18:28:57 +0200

    Filtern der auswahlliste: Es werden nurnoch User angezeigt welche noch nicht assigned wurden.

--- src/app/issue/issue-assigned/issue-assigned.component.ts
+++ src/app/issue/issue-assigned/issue-assigned.component.ts
@@ -29,14 +29,14 @@ export class IssueAssignedComponent extends SubscriptionWrapper implements OnIni
   ngOnInit(): void {
     this.projectId = this.route.snapshot.paramMap.get('projectId');
     this.issueId = this.route.snapshot.paramMap.get('issueId');
-    this.subscribe(forkJoin([this.getAssignedUser(this.issueId), this.getProjectUser(this.projectId)]));
-  }
 
-  // TODO: Evtl die auswahl Liste filtern nach Usern welche noch nicht assigned wurden?
+    this.subscribe(forkJoin([this.getAssignedUser(this.issueId), this.getProjectUser(this.projectId)]), () => this.updateSelectionList());
+  }
 
   // Attributes
   listOfAssignedUsers: IssueAssignedUser[];
   listOfProjectUser: ProjectUser[];
+  filteredSelectionList: ProjectUser[];
   inputValue: ProjectUser;
   compareUserInput(o1: string | ProjectUser, o2: ProjectUser): boolean {
     if (!o1) return false;
@@ -47,7 +47,13 @@ export class IssueAssignedComponent extends SubscriptionWrapper implements OnIni
 
     // Compare Objects
     return o1.user.id.localeCompare(o2.user.id) == 0;
-  };
+  }
+
+  private updateSelectionList(): void {
+    this.filteredSelectionList = this.listOfProjectUser.filter(
+      pUser => !this.listOfAssignedUsers.some(value => value.user.id.localeCompare(pUser.user.id) == 0)
+    );
+  }
 
   // Getters
   private getAssignedUser(issueId: string): Observable<IssueAssignedUser[]> {
@@ -69,7 +75,7 @@ export class IssueAssignedComponent extends SubscriptionWrapper implements OnIni
 
   private getProjectUser(projectId: string): Observable<ProjectUser[]> {
     let filterRoles = (value) => {
-      return value.name.localeCompare("Firma") != 0 && value.name.localeCompare("Kunde") != 0
+      return value.name.localeCompare("Firma") != 0 && value.name.localeCompare("Kunde") != 0;
     }
 
     return this.projectUserService.getProjectUsers(projectId).pipe(
@@ -82,6 +88,7 @@ export class IssueAssignedComponent extends SubscriptionWrapper implements OnIni
   removeAssignedUser(userId: string) {
     this.listOfAssignedUsers = this.listOfAssignedUsers.filter(user => user.user.id.localeCompare(userId) != 0); // Remove User from Cardboard
     this.subscribe(this.assignedService.deleteAssignedUser(this.issueId, userId)); // Remove User from DB
+    this.updateSelectionList();
   }
 
   addAssignedUser() {
@@ -89,6 +96,7 @@ export class IssueAssignedComponent extends SubscriptionWrapper implements OnIni
       return;
 
     let newUser: IssueAssignedUser = { user: this.inputValue.user };
+    newUser.user['avatar'] = `data:image/png;base64,${new Identicons(newUser.user.id, 420).toString()}`
     this.inputValue = null;
 
     if (this.listOfAssignedUsers.some(user => user.user.id.localeCompare(newUser.user.id) == 0)) // TODO: User bereits Assigned (Benachrichtigung an Benutzer)
@@ -96,5 +104,6 @@ export class IssueAssignedComponent extends SubscriptionWrapper implements OnIni
 
     this.listOfAssignedUsers = [...this.listOfAssignedUsers, newUser]; // Add User to Cardboard
     this.subscribe(this.assignedService.updateAssignedUser(this.issueId, newUser.user.id, newUser)); // Add User in DB
+    this.updateSelectionList();
   }
 }
