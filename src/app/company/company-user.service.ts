4198bcb2d44f16b8ed1eb91296796a540f4ecd02
namo2 <Namo_2@gmx.de>
2021-04-14 10:59:52 +0200

    Role und CompanyUser Service eingefügt

---
+++ src/app/company/company-user.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { BaseService } from "../base.service";
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { CompanyUser } from "../interfaces/company/CompanyUser";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyUserService {
+  basicPath: string = '/companies';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  constructor(
+    private router: Router,
+    private base: BaseService,
+    private httpClient: HttpClient
+  ) { }
+
+  getCompanyUsers(companyId: string): Observable<CompanyUser[]> {
+    return this.httpClient
+      .get<CompanyUser[]>(this.base.getUrl + this.basicPath + '/' + companyId + '/users', this.httpOptions)
+      .pipe(catchError(this.base.errorHandle));
+  }
+
+  getCompanyUser(companyId: string, id: string): Observable<CompanyUser> {
+    return this.httpClient
+      .get<CompanyUser>(this.base.getUrl + this.basicPath + '/' + companyId + '/users' + id, this.httpOptions)
+      .pipe(catchError(this.base.errorHandle));
+  }
+
+  createCompanyUser(companyId: string, newCompanyUser: CompanyUser): Observable<CompanyUser> {
+    return this.httpClient
+      .post<CompanyUser>(this.base.getUrl + this.basicPath + '/' + companyId + '/users', newCompanyUser, this.httpOptions)
+      .pipe(catchError(this.base.errorHandle));
+  }
+
+  updateCompanyUser(companyId: string, id: string, newCompanyUser: CompanyUser): Observable<CompanyUser> {
+    return this.httpClient
+      .put<CompanyUser>(this.base.getUrl + this.basicPath + '/' + companyId + '/users' + id, newCompanyUser, this.httpOptions)
+      .pipe(catchError(this.base.errorHandle));
+  }
+}
