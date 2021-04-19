4429a3598af8c303d21f492e491d200566d3cc92
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-04-19 18:20:10 +0200

    Basis Funktionen von EmployeeDashboard

--- src/app/employee/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee/employee-dashboard/employee-dashboard.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { CompanyUserService } from 'src/app/company/company-user.service';
+import { CompanyUser } from 'src/app/interfaces/company/CompanyUser';
+import { SubscriptionWrapper } from 'src/app/SubscriptionWrapper';
 
 @Component({
   selector: 'app-employee-dashboard',
   templateUrl: './employee-dashboard.component.html',
   styleUrls: ['./employee-dashboard.component.less']
 })
-export class EmployeeDashboardComponent implements OnInit {
+export class EmployeeDashboardComponent extends SubscriptionWrapper implements OnInit {
 
-  constructor() { }
+  // Create Button
 
-  ngOnInit(): void {
+  companyId: string;
+  employees: CompanyUser[] = [];
+
+  constructor(private router: Router, private route: ActivatedRoute, private companyUserService: CompanyUserService) {
+    super();
   }
 
+  ngOnInit(): void {
+    this.companyId = this.route.snapshot.paramMap.get('companyId');
+    this.subscribe(this.companyUserService.getCompanyUsers(this.companyId)
+      .pipe(tap(data => this.employees = data)))
+  }
 }
--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 import { EmployeeSettingsComponent } from './employee-settings/employee-settings.component';
 import { EmployeeDashboardComponent } from './employee-dashboard/employee-dashboard.component';
 import { EmployeeRoutingModule } from './employee-routing.module';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   declarations: [EmployeeDashboardComponent, EmployeeSettingsComponent],
   imports: [
     CommonModule,
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    NzTableModule,
+    NzButtonModule,
+    NzIconModule,
   ]
 })
 export class EmployeeModule { }
