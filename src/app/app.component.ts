c3ac1f6567f231e67278ad17b85d3fbbccc0499d
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-04-22 22:29:06 +0200

    Links mit companyId hardcoded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from './auth/auth.service';
+import { Project } from './interfaces/project/Project';
+import { IssueService } from './issue/issue.service';
+import { ProjectService } from './project/project.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +14,21 @@ export class AppComponent {
   title = 'Goose';
   collapsed = false;
 
-  constructor(public authService: AuthService) {
+  projects: Project[] = [];
+  companyId: string;
+
+  constructor(
+    public authService: AuthService,
+    private route: ActivatedRoute,
+    private projectService: ProjectService) {
   }
 
   ngOnInit() {
     this.collapsed = JSON.parse(localStorage.getItem('sidebar_collapse')) ?? false;
+    this.projectService.getProjects(this.companyId).subscribe(data => this.projects = data);
+    // TODO: Update nach URL change
+    this.companyId = this.route.snapshot.paramMap.get('companyId');
+    this.companyId = "6070895a53608b0ba47360f1";
   }
 
   saveCollapse(collapse: boolean): void {
