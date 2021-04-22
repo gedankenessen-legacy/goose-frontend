4438a6fdcb260f61ff7fd227aad29b6403b11fcd
Louis <louisaltm@gmail.com>
2021-04-22 20:05:32 +0200

    auth service aus main

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,25 +2,19 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { BaseService } from '../base.service';
 import { User } from '../interfaces/User';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthService {
   private currentUserSubject: BehaviorSubject<User>;
   public currentUser: Observable<User>;
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-    private baseService: BaseService
-  ) {
-    this.currentUserSubject = new BehaviorSubject<User>(
-      JSON.parse(localStorage.getItem('token'))
-    );
+  constructor(private http: HttpClient, private router: Router, private baseService: BaseService) {
+    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('token')));
     this.currentUser = this.currentUserSubject.asObservable();
   }
 
@@ -35,38 +29,22 @@ export class AuthService {
       }),
     };
 
-    return this.http
-      .post<any>(
-        this.baseService.getUrl + '/auth/signIn',
-        { username, password },
-        httpOptions
-      )
-      .pipe(
-        map((user) => {
-          localStorage.setItem(
-            'token',
-            JSON.stringify({ ...user.user, token: user.token })
-          );
-          this.currentUserSubject.next(user);
-          return user;
-        })
-      );
+    return this.http.post<any>(this.baseService.getUrl + '/auth/signIn', { username, password }, httpOptions)
+      .pipe(tap(data => {
+        localStorage.setItem('token', JSON.stringify({ ...data.user, token: data.token }));
+        this.currentUserSubject.next(data);
+      }));
   }
 
-  loginAfterRegister(
-    id: string,
-    username: string,
-    firstname: string,
-    lastname: string,
-    token: string
-  ) {
+
+  loginAfterRegister(id: string, username: string, firstname: string, lastname: string, token: string) {
     let user: User = {
       id: id,
       username: username,
       firstname: firstname,
       lastname: lastname,
-      token: token,
-    };
+      token: token
+    }
 
     localStorage.setItem('token', JSON.stringify(user));
     this.currentUserSubject.next(user);
@@ -78,4 +56,5 @@ export class AuthService {
     this.currentUserSubject.next(null);
     return true;
   }
+
 }
