d26b8c8558123ddbca2e76624d39b7af8c57a691
Louis <louisaltm@gmail.com>
2021-04-22 20:07:12 +0200

    Auth Interceptor aus main

--- src/app/AuthHttpInterceptor.ts
+++ src/app/AuthHttpInterceptor.ts
@@ -3,25 +3,19 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor,
+  HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth/auth.service';
 
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
 
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
-    return next.handle(
-      request.clone({
-        setHeaders: {
-          Authorization: `Bearer ${this.authService.currentUserValue?.token}`,
-        },
-      })
-    );
+  constructor(private authService: AuthService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return (this.authService.currentUserValue?.token)
+      ? next.handle(request.clone({ setHeaders: { Authorization: `Bearer ${this.authService.currentUserValue.token}` } }))
+      : next.handle(request);
   }
 }
