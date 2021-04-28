8f9e9f2f67ab6dd56b5bb557ecf01c0c760f2a5f
Lint Action <lint-action@samuelmeuli.com>
2021-04-28 14:07:43 +0000

    Fix code style issues with Prettier

--- src/app/project/dashboard/dashboard.component.ts
+++ src/app/project/dashboard/dashboard.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { NzButtonSize } from 'ng-zorro-antd/button';
-import { forkJoin, Observable } from "rxjs";
-import { map, switchMap, tap } from "rxjs/operators";
+import { forkJoin, Observable } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { Project } from '../../interfaces/project/Project';
 import ProjectDashboardContent from '../../interfaces/project/ProjectDashboardContent';
-import { Issue } from "../../interfaces/issue/Issue";
-import { ActivatedRoute, Router } from "@angular/router";
-import { ProjectUser } from "../../interfaces/project/ProjectUser";
+import { Issue } from '../../interfaces/issue/Issue';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProjectUser } from '../../interfaces/project/ProjectUser';
 import { ProjectService } from '../project.service';
 import { ProjectUserService } from '../project-user.service';
 import { IssueService } from 'src/app/issue/issue.service';
-import { SubscriptionWrapper } from "../../SubscriptionWrapper";
-import { Role } from "../../interfaces/Role";
-import { AuthService } from "../../auth/auth.service";
-import { CompanyUser } from "../../interfaces/company/CompanyUser";
+import { SubscriptionWrapper } from '../../SubscriptionWrapper';
+import { Role } from '../../interfaces/Role';
+import { AuthService } from '../../auth/auth.service';
+import { CompanyUser } from '../../interfaces/company/CompanyUser';
 import { CompanyUserService } from 'src/app/company/company-user.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.less']
+  styleUrls: ['./dashboard.component.less'],
 })
 export class DashboardComponent extends SubscriptionWrapper implements OnInit {
   constructor(
@@ -41,7 +41,13 @@ export class DashboardComponent extends SubscriptionWrapper implements OnInit {
 
   ngOnInit(): void {
     this.companyId = this.route.snapshot.paramMap.get('companyId');
-    this.subscribe(forkJoin([this.getCompanyUser(this.companyId), this.getAllResources(this.companyId)]), () => this.processContent());
+    this.subscribe(
+      forkJoin([
+        this.getCompanyUser(this.companyId),
+        this.getAllResources(this.companyId),
+      ]),
+      () => this.processContent()
+    );
   }
 
   // Attributes
@@ -51,35 +57,48 @@ export class DashboardComponent extends SubscriptionWrapper implements OnInit {
   sortColumnProject(a: Project, b: Project): number {
     return a.name.localeCompare(b.name);
   }
-  sortColumnCustomer(a: ProjectDashboardContent, b: ProjectDashboardContent): number {
+  sortColumnCustomer(
+    a: ProjectDashboardContent,
+    b: ProjectDashboardContent
+  ): number {
     return a.customer.lastname.localeCompare(b.customer.lastname);
   }
 
   // Data lists
   listOfProjects: Project[];
-  listOfProjectUsers: Map<string, ProjectUser[]> = new Map<string, ProjectUser[]>();
+  listOfProjectUsers: Map<string, ProjectUser[]> = new Map<
+    string,
+    ProjectUser[]
+  >();
   listOfIssues: Map<string, Issue[]> = new Map<string, Issue[]>();
   listOfDashboardContent: ProjectDashboardContent[];
   // TODO: Refactor: listOfProjects, listOfProjectUsers, listOfIssues werden nur zum erstellen von listOfDashboardContent benötigt
 
   isCompanyAccount(): boolean {
-    return this.loggedInUserCompanyRoles?.some(r => r.name === 'Firma');
+    return this.loggedInUserCompanyRoles?.some((r) => r.name === 'Firma');
   }
 
   private processContent(): void {
     let getCustomer = (projectId: string) => {
-      return this.listOfProjectUsers.get(projectId).filter(
-        user => user.roles.some(role => role.name == 'Kunde'))[0]?.user
-    }
+      return this.listOfProjectUsers
+        .get(projectId)
+        .filter((user) => user.roles.some((role) => role.name == 'Kunde'))[0]
+        ?.user;
+    };
 
     let hasWritePermission = (projectId: string) => {
       let loggedInUser = this.authService.currentUserValue;
-      let user = this.listOfProjectUsers.get(projectId).filter(user => user.user.id === loggedInUser.id)[0];
-      return this.isCompanyAccount() ||
-        (user?.roles.some(r => r.name !== 'Kunde') && user?.roles.some(r => r.name !== "Mitarbeiter (Lesend)")); // Exclude Users with Roles without write permission
-    }
+      let user = this.listOfProjectUsers
+        .get(projectId)
+        .filter((user) => user.user.id === loggedInUser.id)[0];
+      return (
+        this.isCompanyAccount() ||
+        (user?.roles.some((r) => r.name !== 'Kunde') &&
+          user?.roles.some((r) => r.name !== 'Mitarbeiter (Lesend)'))
+      ); // Exclude Users with Roles without write permission
+    };
 
-    this.listOfDashboardContent = this.listOfProjects.map(project => {
+    this.listOfDashboardContent = this.listOfProjects.map((project) => {
       const issues: Issue[] = this.listOfIssues.get(project.id);
 
       return {
@@ -87,36 +106,55 @@ export class DashboardComponent extends SubscriptionWrapper implements OnInit {
         name: project.name,
         customer: getCustomer(project.id),
         issues: issues.length,
-        issuesOpen: issues.filter(issue => issue.state?.phase != 'Abschlussphase').length,
-        hasWritePermission: hasWritePermission(project.id)
-      }
+        issuesOpen: issues.filter(
+          (issue) => issue.state?.phase != 'Abschlussphase'
+        ).length,
+        hasWritePermission: hasWritePermission(project.id),
+      };
     });
   }
 
   // Getters
   private getCompanyUser(companyId: string): Observable<CompanyUser> {
-    return this.companyUserService.getCompanyUser(companyId, this.authService.currentUserValue.id)
-      .pipe(tap(user => this.loggedInUserCompanyRoles = user.roles));
+    return this.companyUserService
+      .getCompanyUser(companyId, this.authService.currentUserValue.id)
+      .pipe(tap((user) => (this.loggedInUserCompanyRoles = user.roles)));
   }
 
-  private getAllResources(companyId: string): Observable<void>  {
+  private getAllResources(companyId: string): Observable<void> {
     return this.projectService.getProjects(companyId).pipe(
-      tap(projects => this.listOfProjects = projects),
+      tap((projects) => (this.listOfProjects = projects)),
       switchMap((projects: Project[]) => {
-        const ids = projects.map(project => project.id);
+        const ids = projects.map((project) => project.id);
 
-        const projectUser = ids.map(
-          id => this.projectUserService.getProjectUsers(id).pipe(tap(projectsUsers => this.listOfProjectUsers.set(id, projectsUsers))));
+        const projectUser = ids.map((id) =>
+          this.projectUserService
+            .getProjectUsers(id)
+            .pipe(
+              tap((projectsUsers) =>
+                this.listOfProjectUsers.set(id, projectsUsers)
+              )
+            )
+        );
 
-        const projectIssues = ids.map(
-          id => this.issueService.getIssues(id).pipe(tap(projectsIssues => this.listOfIssues.set(id, projectsIssues))));
+        const projectIssues = ids.map((id) =>
+          this.issueService
+            .getIssues(id)
+            .pipe(
+              tap((projectsIssues) => this.listOfIssues.set(id, projectsIssues))
+            )
+        );
 
-        return forkJoin([forkJoin(projectUser),forkJoin(projectIssues)]).pipe(map(() => null));
+        return forkJoin([forkJoin(projectUser), forkJoin(projectIssues)]).pipe(
+          map(() => null)
+        );
       })
     );
   }
 
   routeToIssueDashboard(projectId: string): void {
-    this.router.navigateByUrl(`${this.companyId}/projects/${projectId}/issues`).then();
+    this.router
+      .navigateByUrl(`${this.companyId}/projects/${projectId}/issues`)
+      .then();
   }
 }
