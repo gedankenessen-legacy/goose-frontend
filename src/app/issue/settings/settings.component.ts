b35b8779c36882be0e5fc7061298d74689694700
Benjamin Kürten <admin@benjaminkuerten.de>
2021-04-30 00:59:33 +0200

    Fertigstellung IssueSettings

--- src/app/issue/settings/settings.component.ts
+++ src/app/issue/settings/settings.component.ts
@@ -2,16 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Issue } from 'src/app/interfaces/issue/Issue';
 import { User } from 'src/app/interfaces/User';
 import { Predecessor } from 'src/app/interfaces/issue/Predecessor';
-import { IssueDocument } from 'src/app/interfaces/issue/Document';
-import { State } from 'src/app/interfaces/project/State';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IssueService } from '../issue.service';
 import { IssueRelevantDocuments } from 'src/app/interfaces/issue/IssueRelevantDocuments';
 import { IssueAssignedUsersService } from '../issue-assigned-users.service';
-import { first } from 'rxjs/operators';
 import { IssuePredecessorService } from '../issue-predecessors.service';
 import { IssueDetailsService } from '../issue-details.service';
 import { IssueAssignedUser } from "../../interfaces/issue/IssueAssignedUser";
+import { ProjectService } from 'src/app/project/project.service';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-settings',
@@ -19,33 +18,39 @@ import { IssueAssignedUser } from "../../interfaces/issue/IssueAssignedUser";
   styleUrls: ['./settings.component.less'],
 })
 export class SettingsComponent implements OnInit {
-  visibleInput: string;
-  stateName: string;
-
-  constructor(private router: Router, private issueService: IssueService, private issueDetailsService: IssueDetailsService, private issueAssignedUsersService: IssueAssignedUsersService, private issuePredecessorService: IssuePredecessorService, private route: ActivatedRoute) { }
-
+  
+  visibleInput: string = "";
+  stateActive: boolean = true;
   companyId: string;
   projectId: string;
   issueId: string;
+
+  constructor(private router: Router, private issueService: IssueService, private issueDetailsService: IssueDetailsService, private issueAssignedUsersService: IssueAssignedUsersService, private issuePredecessorService: IssuePredecessorService, private route: ActivatedRoute, private projectService: ProjectService, private modal: NzModalService) { }
+
   ngOnInit(): void {
     this.companyId = this.route.snapshot.paramMap.get('companyId');
     this.projectId = this.route.snapshot.paramMap.get('projectId');
     this.issueId = this.route.snapshot.paramMap.get('issueId');
 
-
+    //Load selected issue
     if (this.issueId != null) {
+      //Get issue details
       this.getIssue();
+      //Get assigned users
       this.getAssignedUsers();
+      //Get predecessors
       this.getAllPredecessors();
+      //get documents
       this.getAllDocuments();
     }
   }
 
-   issue: Issue = {
+  //Basic issue model
+  issue: Issue = {
     "createdAt": undefined,
     "state": {
       "id": "",
-      "name": "",
+      "name": "Überprüfung",
       "phase": "",
       "userGenerated": false,
     },
@@ -64,50 +69,21 @@ export class SettingsComponent implements OnInit {
       "visibility": true,
       "relevantDocuments": []
     }
-   };
+  };
 
-
-
+  //Load existing issue
   getIssue() {
     this.issueDetailsService.getIssue(this.projectId, this.issueId).subscribe(
       (data) => {
         this.issue = data;
-
-
-        console.log(data);
         if (this.issue.issueDetail.visibility) {
           this.visibleInput = 'extern';
         } else {
           this.visibleInput = 'intern';
         }
 
-        /** DEBUG FOR NULL VALUE IN TICKET */
-        /** BACKEND SET TICKET STATE, CLIENT, AUTHOR TO NULL - IN DEV */
-        if (this.issue.state == null) {
-          this.issue.state = {
-            "id": "",
-            "name": "ueberpruefen",
-            "phase": ""
-          }
-        }
+        this.stateActive = false;
 
-        if (this.issue.client == null) {
-          this.issue.client = {
-            "id": "",
-            "firstname": "",
-            "lastname": ""
-          }
-        }
-
-        if (this.issue.author == null) {
-          this.issue.author = {
-            "id": "",
-            "firstname": "",
-            "lastname": ""
-          }
-        }
-        /** END OF DEBUG */
-      
         (error) => {
           console.error(error);
         }
@@ -115,39 +91,76 @@ export class SettingsComponent implements OnInit {
     );
   }
 
+  //Save issue
   submitForm() {
-    //Set visibilty state
-    if (this.visibleInput === 'extern') {
-      this.issue.issueDetail.visibility = true;
+    if (this.issue.issueDetail.name.length > 0 && this.issue.state.name.length > 0 && this.issue.issueDetail.type.length > 0 && this.visibleInput.length > 0) {
+
+      //Set visibilty state
+      if (this.visibleInput === 'extern') {
+        this.issue.issueDetail.visibility = true;
+      } else {
+        this.issue.issueDetail.visibility = false;
+      }
+
+      //Set relevant documents
+      this.issue.issueDetail.relevantDocuments = this.generateStringArray(this.listOfDocuments);
+
+      //Update issue
+      if (this.issueId != null) {
+        this.issueService.updateIssue(this.projectId, this.issueId, this.issue).subscribe(
+          (data) => {
+            this.router.navigateByUrl(`${this.companyId}/projects/${this.projectId}/issues`);
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+
+
+        //Create new issue
+      } else {
+        this.projectService.getProject(this.companyId, this.projectId).subscribe(
+          (dataProject) => {
+            //Set Client
+            this.issue.client = {
+              "id": JSON.parse(localStorage.getItem('token')).id,
+              "firstname": JSON.parse(localStorage.getItem('token')).firstname,
+              "lastname": JSON.parse(localStorage.getItem('token')).lastname
+            }
+
+            //Set Author
+            this.issue.author = {
+              "id": JSON.parse(localStorage.getItem('token')).id,
+              "firstname": JSON.parse(localStorage.getItem('token')).firstname,
+              "lastname": JSON.parse(localStorage.getItem('token')).lastname
+            }
+
+            //Set Project
+            this.issue.project = {
+              "name": dataProject.name,
+              "id": this.projectId
+            }
+
+            //Create new issue
+            this.issueService.createIssue(this.projectId, this.issue).subscribe(
+              (data) => {
+                this.router.navigateByUrl(`${this.companyId}/projects/${this.projectId}/issues`);
+              },
+              (error) => {
+                console.error(error);
+              }
+            );
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+      }
     } else {
-      this.issue.issueDetail.visibility = false;
-    }
-
-    //Set relevant documents
-    this.issue.issueDetail.relevantDocuments = this.generateStringArray(this.listOfDocuments);
-
-    //Update issue
-    if (this.issueId != null) {
-      this.issueService.updateIssue(this.projectId, this.issueId, this.issue).subscribe(
-        (data) => {
-          this.router.navigateByUrl(`${this.companyId}/projects/${this.projectId}/issues`);
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
-
-      
-    //Create new issue
-    } else {
-      this.issueService.createIssue(this.projectId, this.issue).subscribe(
-        (data) => {
-          this.router.navigateByUrl(`${this.companyId}/projects/${this.projectId}/issues`);
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
+      this.modal.error({
+        nzTitle: 'Fehler beim speichern des Tickets',
+        nzContent: 'Bitte füllen Sie alle Pflichtfelder aus'
+      });
     }
   }
 
