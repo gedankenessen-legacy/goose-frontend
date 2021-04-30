6b0480bb66269a78c423b98b2117366dde4dc83e
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-04-30 11:02:36 +0200

    Fix imports

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -6,15 +6,26 @@ import { CustomerRoutingModule } from './customer-routing';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { CustomerSettingsComponent } from './customer-settings/customer-settings.component';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 @NgModule({
   declarations: [CustomerDashboardComponent, CustomerSettingsComponent],
   imports: [
     CommonModule,
     CustomerRoutingModule,
-    NzTableModule,
     NzButtonModule,
+    NzTableModule,
     NzIconModule,
+    NzInputModule,
+    NzGridModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzLayoutModule,
   ],
 })
 export class CustomerModule { }
