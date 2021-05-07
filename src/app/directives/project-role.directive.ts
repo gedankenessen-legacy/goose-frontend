06a994f1edc94e218652dc132f3f5d49a0faf8d5
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-05-07 23:53:24 +0200

    ProjectRole Funktionen

--- src/app/directives/project-role.directive.ts
+++ src/app/directives/project-role.directive.ts
@@ -1,10 +1,34 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { ProjectUserService } from '../project/project-user.service';
+import { IdentityService } from '../services/identity.service';
+import { SubscriptionWrapper } from '../SubscriptionWrapper';
 
 @Directive({
   selector: '[appProjectRole]'
 })
-export class ProjectRoleDirective {
+export class ProjectRoleDirective extends SubscriptionWrapper {
+  @Input() roles: string[];
 
-  constructor() { }
+  constructor(
+    private identity: IdentityService,
+    private users: ProjectUserService,
+    private elementRef: ElementRef) {
+    super();
+  }
 
+  ngOnInit() {
+    this.subscribe(
+      forkJoin([
+        this.identity.projectId.asObservable(),
+        this.identity.userId.asObservable()
+      ]).pipe(
+        switchMap(data => this.users.getProjectUser(...data)),
+        tap(user =>
+          this.elementRef.nativeElement.style.display = user.roles.some(r => this.roles.includes(r.id)) ? 'block' : 'none'
+        )
+      )
+    );
+  }
 }
