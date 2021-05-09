b0c39e5d4d671ff20c06f8f553e8da47d31b6739
sfuerder <sfuerder@stud.hs-offenburg.de>
2021-05-09 14:52:45 +0200

    Prioriäts Komponente implementiert

--- src/app/issue/dashboard/priority/priority.component.ts
+++ src/app/issue/dashboard/priority/priority.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-priority',
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./priority.component.less'],
 })
 export class PriorityComponent implements OnInit {
-  constructor() {}
+  @Input() public priority: number;
 
-  ngOnInit(): void {}
+  color: string = "green";
+  constructor() { }
+
+  ngOnInit(): void {
+    if(this.priority<=3)
+      this.color = "green";
+    else if(this.priority<=7)
+      this.color = "orange";
+    else
+      this.color = "red";
+  }
+
 }
