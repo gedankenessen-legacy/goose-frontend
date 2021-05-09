9d7a31b8862c0e307bdd7b15cdbc94136e712570
mschwer1 <morit@141.79.52.171>
2021-05-09 13:41:50 +0200

    IssueDashboard2 Branch erstellt

--- src/app/issue/dashboard/dashboard.component.ts
+++ src/app/issue/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Issue } from 'src/app/interfaces/issue/Issue';
 import { IssueService } from '../issue.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NzTableFilterList } from 'ng-zorro-antd/table';
+import { ProjectUser } from 'src/app/interfaces/project/ProjectUser';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,7 +15,8 @@ export class DashboardComponent implements OnInit {
   constructor(
     private issueService: IssueService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   cardDesign: boolean = false;
@@ -48,5 +52,60 @@ export class DashboardComponent implements OnInit {
     this.cardDesign = !this.cardDesign;
     if (this.cardDesign) this.btnCardDesignTitle = 'Table Design';
     else this.btnCardDesignTitle = 'Card Design';
+  }
+
+  listOfProjectUsers: ProjectUser[] = [];
+
+  public cannotCreateIssue(): Boolean {
+    let loggedInUser = this.authService.currentUserValue;
+    let user = this.listOfProjectUsers.filter(
+      (user) => user.user.id === loggedInUser.id
+    )[0];
+    return (
+      user?.roles.some((r) => r.name === 'Kunde') ||
+      user?.roles.some((r) => r.name === 'Mitarbeiter (Lesend)')
+    ); // Exclude Users with Roles without write permission
+  }
+
+  sortColumnIssue(a: Issue, b: Issue): number {
+    return a.issueDetail.name.localeCompare(b.issueDetail.name);
+  }
+
+  sortColumnProgress(a: Issue, b: Issue): number {
+    return a.issueDetail.progress - b.issueDetail.progress;
+  }
+
+  sortColumnStart(a: Issue, b: Issue): number {
+    return (
+      a.issueDetail.startDate.getTime() - b.issueDetail.startDate.getTime()
+    );
+  }
+
+  sortColumnDeadline(a: Issue, b: Issue): number {
+    return a.issueDetail.endDate.getTime() - b.issueDetail.endDate.getTime();
+  }
+
+  sortColumnState(a: Issue, b: Issue): number {
+    return a.state.name.localeCompare(b.state.name);
+  }
+
+  listOfFilterStates: NzTableFilterList;
+
+  filterState(list: string[], item: Issue): Boolean {
+    return list.some((name) => item.state.name.indexOf(name) !== -1);
+  }
+
+  listOfFilterPriorities: NzTableFilterList;
+
+  filterPriority(list: number[], item: Issue): Boolean {
+    return list.some((name) => name == item.issueDetail.priority);
+  }
+
+  listOfFilterWorkers: NzTableFilterList;
+
+  filterWorker(list: string[], item: Issue): Boolean {
+    return list.some((name) =>
+      item.assignedUsers.some((user) => user.id == name)
+    );
   }
 }
