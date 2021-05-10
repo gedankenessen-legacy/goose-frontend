b16254eb0f189c8d8437ea5704232b3b42da7cd9
Lint Action <lint-action@samuelmeuli.com>
2021-05-10 19:49:16 +0000

    Fix code style issues with Prettier

--- src/app/shared/avatar/avatar.component.ts
+++ src/app/shared/avatar/avatar.component.ts
@@ -4,16 +4,15 @@ import * as Identicons from 'identicon.js';
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
-  styleUrls: ['./avatar.component.less']
+  styleUrls: ['./avatar.component.less'],
 })
 export class AvatarComponent implements OnInit {
-
   @Input() id: string;
 
-  constructor() { }
-  ngOnInit(): void { }
+  constructor() {}
+  ngOnInit(): void {}
 
   source(): string {
-    return `data:image/png;base64,${new Identicons(this.id, 420).toString()}`
+    return `data:image/png;base64,${new Identicons(this.id, 420).toString()}`;
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,9 @@ import { CommonModule } from '@angular/common';
 import { AvatarComponent } from './avatar/avatar.component';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 
-
-
 @NgModule({
   declarations: [AvatarComponent],
-  imports: [
-    CommonModule,
-    NzAvatarModule
-  ],
-  exports: [AvatarComponent]
+  imports: [CommonModule, NzAvatarModule],
+  exports: [AvatarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
