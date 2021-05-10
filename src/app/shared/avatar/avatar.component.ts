61a6eeaa4f7e20b1d405bd1bef2efe01c7b4b735
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-05-10 21:48:16 +0200

    Füge Avatar Komponente hinzu

---
+++ src/app/shared/avatar/avatar.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import * as Identicons from 'identicon.js';
+
+@Component({
+  selector: 'app-avatar',
+  templateUrl: './avatar.component.html',
+  styleUrls: ['./avatar.component.less']
+})
+export class AvatarComponent implements OnInit {
+
+  @Input() id: string;
+
+  constructor() { }
+  ngOnInit(): void { }
+
+  source(): string {
+    return `data:image/png;base64,${new Identicons(this.id, 420).toString()}`
+  }
+}
