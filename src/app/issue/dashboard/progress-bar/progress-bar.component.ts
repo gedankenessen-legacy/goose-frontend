b2468496ea1b42ed17701f9bb56b15cc35e2b886
sfuerder <sfuerder@stud.hs-offenburg.de>
2021-05-12 13:47:07 +0200

    Berechnung des erwarteten fortschritts in der ProgressBar Component

--- src/app/issue/dashboard/progress-bar/progress-bar.component.ts
+++ src/app/issue/dashboard/progress-bar/progress-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { IssueTimeSheet } from 'src/app/interfaces/issue/IssueTimeSheet';
 
 @Component({
   selector: 'app-progress-bar',
@@ -6,19 +7,33 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./progress-bar.component.less'],
 })
 export class ProgressBarComponent implements OnInit {
-  @Input() public currentProgress: number;
-  @Input() public expectedProgress: number;
+  @Input() public progress: number;
+  @Input() public expectedTime: number;
+  @Input() public timeSheets: IssueTimeSheet[];
 
   color: string = 'green';
 
   constructor() {}
 
   ngOnInit(): void {
-    const diffProgress: number = this.expectedProgress - this.currentProgress;
+    const expectedProgress = (this.calcTimeNeeded() /( this.expectedTime*3600))*100;
+    
+    const diffProgress: number = expectedProgress - this.progress;
 
     if (diffProgress <= 0) this.color = 'green';
     else if (diffProgress <= 25) this.color = 'orange';
     else if (diffProgress > 25) this.color = 'red';
     else this.color = 'gray';
+  }
+
+  calcTimeNeeded(): number{
+    let sumInSeconds: number = 0;
+    this.timeSheets?.forEach((timeSheet) => {
+      const diffInSeconds: number = (new Date(timeSheet.end).valueOf() - new Date(timeSheet.start).valueOf()) / 1000;
+      sumInSeconds += diffInSeconds;
+      
+    })
+    
+    return sumInSeconds;
   }
 }
