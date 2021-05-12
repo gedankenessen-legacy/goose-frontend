a742166293ff8aaded4af1280bc27c80d1326630
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-05-12 22:04:41 +0200

    Erstelle avatar-pipe

---
+++ src/app/shared/avatar/avatar.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as Identicons from 'identicon.js';
+
+@Pipe({
+  name: 'avatar'
+})
+export class AvatarPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return `data:image/png;base64,${new Identicons(value, 420).toString()}`;
+  }
+}
