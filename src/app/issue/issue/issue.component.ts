20b3475735ba21d7759cc4d3bc09566ee0538ae3
namo2 <Namo_2@gmx.de>
2021-06-23 04:09:37 +0200

    Implemented T78 + T21

--- src/app/issue/issue/issue.component.ts
+++ src/app/issue/issue/issue.component.ts
@@ -179,8 +179,18 @@ export class IssueComponent extends SubscriptionWrapper implements OnInit {
 
   customerIsAuthor(): boolean {
     return (
-      this.hasRole('Kunde') && this.issue?.author.id == this.currentUser?.id
+      this.hasRole('Kunde') && this.issue?.author.id === this.authService.currentUserValue.id
     );
+  }
+
+  canEditIssue(): boolean {
+    return !this.isPhase('Abschlussphase') &&
+      (this.customerIsAuthor() ||
+        (!this.hasRole('Mitarbeiter (Lesend)') && !this.hasRole('Kunde')));
+  }
+
+  canCancelIssue(): boolean {
+    return !this.isState('Abgebrochen') && (this.hasRole("Kunde") || this.hasRole("Projektleiter") || this.hasRole("Firma"))
   }
 
   cancelIssue(): void {
